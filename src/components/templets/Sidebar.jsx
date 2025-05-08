e208fb5bb1f120aed435a7e9107fbf25e3af3a5a
Akhil <konda.akhil456@gmail.com>
2025-05-08 10:34:44 +0530

    Topnav ui complete

--- src/components/templets/Sidebar.jsx
+++ src/components/templets/Sidebar.jsx
@@ -6,29 +6,29 @@ function Sidebar() {
     <>
       <div className="w-[20%] h-full border-r-2 border-white p-5">
         <div className="text-3xl">
-          <i class="ri-film-fill mr-2 text-[#6556CD]"></i>
+          <i className="ri-film-fill mr-2 text-[#6556CD]"></i>
           <span className="text-white font-bold ">Cinebyte</span>
         </div>
         <nav className="flex flex-col gap-4 ">
           <h1 className="text-white text-xl font-semibold mt-5">New Feed</h1>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-blaze-fill mr-2"></i>
+            <i className="ri-blaze-fill mr-2"></i>
             Trending
           </Link>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-bard-line mr-2"></i>
+            <i className="ri-bard-line mr-2"></i>
             Popular
           </Link>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-movie-2-line mr-2"></i>
+            <i className="ri-movie-2-line mr-2"></i>
             Movies
           </Link>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-tv-line mr-2"></i>
+            <i className="ri-tv-line mr-2"></i>
             TV Shows
           </Link>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-user-line mr-2"></i>
+            <i className="ri-user-line mr-2"></i>
             People
           </Link>
         </nav>
@@ -36,11 +36,11 @@ function Sidebar() {
         <nav className="flex flex-col gap-4 ">
           <h1 className="text-white text-xl font-semibold mt-5">Website info</h1>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-file-info-line mr-2"></i>
+            <i className="ri-file-info-line mr-2"></i>
             About Cinebyte
           </Link>
           <Link className="hover:bg-[#6556CD] text-lg hover:text-white text-zinc-400 px-2 py-2 rounded-md duration-200">
-          <i class="ri-mail-line mr-2"></i>
+            <i className="ri-mail-line mr-2"></i>
             Contact Us
           </Link>
         </nav>
--- src/components/templets/Topnav.jsx
+++ src/components/templets/Topnav.jsx
@@ -1,49 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Topnav() {
+  const [search, setSearch] = useState("");
+  console.log(search);
   return (
-    <div className=' relative w-100% h-[10vh] flex justify-center items-center '>
-      <i class="ri-search-line text-zinc-300 text-2xl"></i>
-       <input type="text" placeholder='search' className='outline-none border-none py-2 mx-5 text-white w-[50%]' />
-       <i class="text-zinc-300 text-2xl ri-close-line"></i>
-       <div className=' w-[50%] h-[50vh] absolute overflow-auto top-[90%]'>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        <Link className='inline-block border-b-2 border-zinc-400 text-white  bg-zinc-600 py-8 flex justify-start items-center px-2 w-full'>
-        <img src="" alt="" />
-        <span>movies from 2024 blockbuster</span>
-        </Link>
-        
-       </div>
+    <div className="relative w-100% h-[10vh] flex justify-start ml-[15%] items-center">
+      <i className="ri-search-line text-zinc-300  text-2xl"></i>
+      <input
+        onChange={(e) =>setSearch(e.target.value)}
+        value={search}
+        type="text"
+        placeholder="search"
+        className="outline-none bg-transparent px-2 border-none py-2 mx-5 text-white w-[50%]"
+      />
+      {search.length > 0 && <i onClick={()=>setSearch('')} className="text-zinc-300 text-2xl ri-close-line"></i> }
+      
+      <div className="w-[50%] max-h-[50vh] absolute overflow-auto top-[90%]">
+        {/* <Link className="inline-block border-b-2 rounded-md hover:bg-zinc-700 border-zinc-400 text-white bg-zinc-600 py-8 flex justify-start items-center px-2 w-full">
+          <img src={null} alt="movie poster" />
+          <span>movies from 2024 blockbuster</span>
+        </Link> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Topnav
+export default Topnav;
